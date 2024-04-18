765fad2468bd9e8d5bac50cb70a2ab646ad10a95
Olexandr Veselyi <olexandr.veseluy@gmail.com>
2024-04-18 20:49:31 +0300

    v0.0.5

--- src/main.js
+++ src/main.js
@@ -23,3 +23,51 @@ const swiper = new Swiper('.category-list-wrap', {
     },
   },
 });
+
+const shortsSwiper = new Swiper('.shorts-list-container', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerGroup: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const countDownDate = new Date('Dec 1, 2024 00:00:00').getTime();
+setInterval(() => {
+  const now = new Date().getTime();
+
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerHTML = days < 10 ? '0' + days : days;
+  document.getElementById('hours').innerHTML = hours < 10 ? '0' + hours : hours;
+  document.getElementById('minutes').innerHTML =
+    minutes < 10 ? '0' + minutes : minutes;
+  document.getElementById('seconds').innerHTML =
+    seconds < 10 ? '0' + seconds : seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('demo').innerHTML = 'EXPIRED';
+  }
+}, 1000);
